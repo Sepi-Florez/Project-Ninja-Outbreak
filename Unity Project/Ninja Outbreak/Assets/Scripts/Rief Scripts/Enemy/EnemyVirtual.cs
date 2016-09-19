3ecc3743f0be95ff889cf530f25144400f153f55
Rief Haalboom <superrief@hotmail.com>
2016-09-19 16:45:21 +0200

    Begin Enemy

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/EnemyVirtual.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyVirtual : MonoBehaviour {
+
+    public float moveSpeed;
+    public bool onPatrol;
+    public GameObject patrolPoint;
+    public GameObject patrolPointOne;
+    public GameObject patrolPointTwo;
+    public GameObject detectObj;
+
+    public abstract void Movement();
+
+    public abstract void Detect();
+
+}
---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/StandardEnemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandardEnemy : EnemyVirtual {
+
+    public bool detected;
+    public Transform alarmObj;
+
+    public void Start () {
+        patrolPoint = patrolPointOne;
+    }
+
+    public void Update () {
+        Movement ();
+    }
+
+    public override void Movement () {
+        float moveTo = moveSpeed * Time.deltaTime;
+        if (detected == true) {
+            transform.position = Vector3.MoveTowards (transform.position, alarmObj.position, moveTo * 2);
+        }
+        if (onPatrol == true) {
+            transform.position = Vector3.MoveTowards (transform.position, patrolPoint.transform.position, moveTo);
+            if (transform.position == patrolPoint.transform.position) {
+                if (patrolPoint == patrolPointOne) {
+                    patrolPoint = patrolPointTwo;
+                }
+                else {
+                    patrolPoint = patrolPointOne;
+                }
+            }
+        }
+    }
+    public override void Detect () {
+    }
+
+}
