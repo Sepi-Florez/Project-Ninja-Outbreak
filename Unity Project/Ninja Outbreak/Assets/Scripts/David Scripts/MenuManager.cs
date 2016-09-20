a37129a385743a6b802ede7444e06d39b3104517
David Vink <david.vink@hotmail.com>
2016-09-20 15:29:19 +0200

    Main Menu Work

--- Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/MenuManager.cs
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/MenuManager.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject[] canvas;
+	public Animator anim;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void menu (int i) {
+		switch(i){
+			case 0:
+				print("Start Game");
+				break;
+			case 1: 
+				print("Continue from last save");
+				break;
+			case 2:
+				print("Load Game");
+				break;
+			case 3:
+				//canvas[0].SetActive(false);
+				anim.SetTrigger ("Move");
+				//canvas[1].SetActive(true);
+				break;
+			case 4:
+				canvas[0].SetActive(false);
+				canvas[2].SetActive(true);
+				break;
+			case 5:
+				canvas[0].SetActive(false);
+				canvas[3].SetActive(true);
+				break;
+			case 6:
+				Application.Quit();
+				break;
+		}
 	}
 }
