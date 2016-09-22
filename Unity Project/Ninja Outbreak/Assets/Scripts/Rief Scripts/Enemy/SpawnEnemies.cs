c308744b4afdda0f2c3e882f32139f9c3cefb759
Rief Haalboom <superrief@hotmail.com>
2016-09-22 15:24:23 +0200

    Standard Enemy spawn

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/SpawnEnemies.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemies : MonoBehaviour{
+
+    public bool spawnOn = false;
+    public GameObject theEnemy;
+    public int spawnLimit = 4;
+    public int maxSpawn =  7;
+    public int minSpawn = -7;
+
+    void Start () {
+        
+    }
+	
+	void Update () {
+    }
+    public void Spawn () {
+
+        if (spawnOn == true) {
+            for (int i = 0; i < spawnLimit; i++) {
+                Vector3 xRange = new Vector3 (Random.Range (minSpawn, maxSpawn), transform.position.y, transform.position.z);
+                Instantiate (theEnemy, xRange, Quaternion.identity);
+                }
+            }
+        }
+}
