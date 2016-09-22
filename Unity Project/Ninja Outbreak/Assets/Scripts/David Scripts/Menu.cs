615e557399d22a8bfdcfe46aff0d192c90a4d377
David Vink <david.vink@hotmail.com>
2016-09-22 15:26:21 +0200

    Main Menu work

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+	
+	private Animator anim;
+	private CanvasGroup canvasGroup;
+
+	public void Awake(){
+		anim = GetComponent<Animator>();
+		canvasGroup = GetComponent<CanvasGroup>();
+	}
+	
+	public void menu (int i) {
+		switch(i){
+			case 0:
+				print("Start Game");
+				break;
+			case 1: 
+				print("Continue from last save");
+				break;
+			case 2:
+				print("Load Game");
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				Application.Quit();
+				break;
+			
+		}
+	}
+}
