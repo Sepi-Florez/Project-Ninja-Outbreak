cb33e92e217563c7d729c95216422a02a88fdf54
Paige <paigebemelmans@hotmail.com>
2016-09-23 14:13:01 +0200

    Scripts

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Paige Scripts/MouseCursor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor : MonoBehaviour
+{
+    public Texture2D cursorImage;
+
+    public int cursorWidth = 32;
+    public int cursorHeight = 32;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
+    }
+    public void Big()
+    {
+        cursorHeight = 45;
+        cursorWidth = 45;
+    }
+    public void Normal()
+    {
+        cursorHeight = 32;
+        cursorWidth = 32;
+    }
+}
---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Paige Scripts/Shuriken.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shuriken : MonoBehaviour {
+
+    public Transform shurikenSpawn;
+    public GameObject shuriken;
+    public int shurikendamage;
+    void Start () {
+
+    }
+	//cooldown
+	void Update () {
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Vector3 sp = Camera.main.WorldToScreenPoint(shurikenSpawn.position);
+            Vector3 dir = (Input.mousePosition - sp).normalized;
+            shuriken.GetComponent<Rigidbody2D>().AddForce(dir * 400);
+
+            //Instantiate(shuriken , shurikenSpawn.position, Quaternion.identity);
+            
+        }
+    
+    }
+    void OnCollissionEnter()
+    {
+        //Destroy(shuriken);
+    }
+}
