d28c0d14162b9c85e515e6567a9e31d09d4f763d
Rief Haalboom <superrief@hotmail.com>
2016-09-23 13:41:23 +0200

    StandardEnemy af

--- Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/SpawnEnemies.cs
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/SpawnEnemies.cs
@@ -2,26 +2,45 @@
 using System.Collections;
 
 public class SpawnEnemies : MonoBehaviour{
-
-    public bool spawnOn = false;
-    public GameObject theEnemy;
-    public int spawnLimit = 4;
-    public int maxSpawn =  7;
-    public int minSpawn = -7;
+    //moet op de AlertGuard
+    public bool spawnOn = false; //false laten.
+    public GameObject theEnemy; //enemy die gespawned moet worden.
+    public int spawnLimit = 4; //limiet van hoeveel er gespawned kunnen worden.
+    public Vector3 spawnNum; //aanpassen als je de distance van de spawnpoint wil aanpassen (ALLEEN DE X-AS).
+    private bool maxSpawn; //laten zoals het is.
+    private bool minSpawn; //laten zoals het is.
 
     void Start () {
-        
     }
 	
 	void Update () {
     }
-    public void Spawn () {
-
+    public void SpawnActive () {
+        maxSpawn = true;
         if (spawnOn == true) {
+            StartCoroutine (Spawning ());
+        }
+    }
+    IEnumerator Spawning () { 
             for (int i = 0; i < spawnLimit; i++) {
-                Vector3 xRange = new Vector3 (Random.Range (minSpawn, maxSpawn), transform.position.y, transform.position.z);
-                Instantiate (theEnemy, xRange, Quaternion.identity);
+                Vector3 maxSpawnLoc = new Vector3 (transform.position.x, transform.position.y, transform.position.z) + spawnNum;
+                Vector3 minSpawnLoc = new Vector3 (transform.position.x, transform.position.y, transform.position.z) - spawnNum;
+                if (maxSpawn == true) {
+                    GameObject enemySpawn = Instantiate (theEnemy, maxSpawnLoc, Quaternion.identity) as GameObject;
+                    enemySpawn.transform.LookAt (gameObject.transform);
+                yield return new WaitForSeconds (1);
+                    maxSpawn = false;
+                    minSpawn = true;
+                }
+                else {
+                    if (minSpawn == true) {
+                        GameObject enemySpawn = Instantiate (theEnemy, minSpawnLoc, Quaternion.identity) as GameObject;
+                        enemySpawn.transform.LookAt (gameObject.transform);
+                    yield return new WaitForSeconds (1);
+                        maxSpawn = true;
+                        minSpawn = false;
+                    }
                 }
             }
         }
-}
+    }
