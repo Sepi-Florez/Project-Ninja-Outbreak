18d0451366b8f5ccf58c6a98a7fc096dd7b4351f
David Vink <david.vink@hotmail.com>
2016-09-27 15:05:18 +0200

    InGameMenu work
    Added the UI objects and made it appear and dissapear with escape. Will
    work on Collections next.

--- Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/MenuManager.cs
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/MenuManager.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
-	
+    public bool Main;
+    public bool gameToggle;
 	Animator anim;
 	public GameObject[] menus;
 	GameObject currentMenu;
@@ -11,9 +12,14 @@ public class MenuManager : MonoBehaviour {
 			menus[a].SetActive(false);
 		}
 		currentMenu = menus[0];
-		currentMenu.SetActive(true);
-		anim = currentMenu.GetComponent<Animator>();
-		anim.SetBool("IsOpen",true);
+        anim = currentMenu.GetComponent<Animator>();
+        currentMenu.SetActive(true);
+        if (Main == true) {
+            
+            anim.SetBool("IsOpen", true);
+        }
+        
+        
 	}
 	public void ChangeMenu (GameObject menu) {
 		anim.SetBool("IsOpen", false);
@@ -24,12 +30,28 @@ public class MenuManager : MonoBehaviour {
 		currentMenu = menu;
 	}
 	public void Update () {
-		if(Input.GetButtonDown("Cancel")){
-			print("back");
-			anim.SetBool("IsOpen", false);
-			currentMenu = menus[0];
-			anim = currentMenu.GetComponent<Animator>();
-			anim.SetBool("IsOpen", true);
-		}
+        Escape();
 	}
+    public void Escape() {
+        if (Input.GetButtonDown("Cancel")) {
+            if (Main == false) {
+                InGameChange();
+            }
+            else {
+                MainMenuChange();
+            }
+        }
+    }
+    public void InGameChange () {
+        gameToggle = !gameToggle;
+        transform.GetChild(0).gameObject.SetActive(gameToggle);
+        anim.SetBool("IsOpen", gameToggle);
+        print("Escape");
+    }
+    public void MainMenuChange () {
+        anim.SetBool("IsOpen", false);
+        currentMenu = menus[0];
+        anim = currentMenu.GetComponent<Animator>();
+        anim.SetBool("IsOpen", true);
+    }
 }
