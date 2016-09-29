951d4c382d52362450946aacb5341aa89d4259b2
WalterAHulsebos <treemsi@hotmail.com>
2016-09-29 14:42:22 +0100

    explosionfix
    fix (not the fade bug)

---
+++ Unity Project/Ninja Outbreak/Assets/Explosion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    private float fadePerSecond;
+    private void Update ()
+    {
+        var material = GetComponent<Renderer>().material;
+        //var color = material.color;
+        var color2 = material.GetColor("_Color");
+
+        material.color = new Color(color2.r, color2.g, color2.b, color2.a - (fadePerSecond * Time.deltaTime));
+        if (transform.localScale.y <= 20)
+        {
+            transform.localScale += new Vector3(fadePerSecond, fadePerSecond, fadePerSecond) * Time.deltaTime;
+        }
+        else
+        {
+            //Destroy(transform.gameObject);
+        }
+    }
+}
