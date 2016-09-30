34f5e0d3ac21d1ceb201aba46033bce886867b20
WalterAHulsebos <treemsi@hotmail.com>
2016-09-30 09:14:24 +0100

    Explosions in correcte map geplaats... -__-
    wauw

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Walter Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+class Explosion : MonoBehaviour
+{
+    [SerializeField]
+    private float fadePerSecond;
+    private bool f1;
+    private void Update ()
+    {
+        var material = GetComponent<Renderer>().material;
+        //var color = material.color;
+        var color2 = material.GetColor("_Color");
+        if (Input.GetButton("Jump")) { f1 = true; }
+        if (f1 == true)
+        {
+            material.color = new Color(color2.r, color2.g, color2.b, color2.a - (fadePerSecond/10 * Time.deltaTime));
+            if (transform.localScale.y <= 20)
+            {
+                transform.localScale += new Vector3(fadePerSecond, fadePerSecond, fadePerSecond) * Time.deltaTime;
+            }
+            else
+            {
+                Destroy(transform.gameObject);
+            }
+        }
+    }
+}
