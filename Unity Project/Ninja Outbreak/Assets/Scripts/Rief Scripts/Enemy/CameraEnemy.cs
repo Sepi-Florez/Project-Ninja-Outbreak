e7075e6ca9b08f5078d403c2f61a07f37739275b
Rief Haalboom <superrief@hotmail.com>
2016-11-11 15:46:38 +0100

    CameraEnemy af

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/CameraEnemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEnemy : MonoBehaviour {
+
+    public GameObject player;
+    public bool detected;
+    public bool canSpawn;
+	
+    void Start () {
+        canSpawn = true;
+    }
+	void Update () {
+        Detection ();
+	}
+    void Detection () {
+        if (detected == true) {
+            StartCoroutine (SpawnEnemies ());
+        }
+    }
+    IEnumerator SpawnEnemies () {
+        detected = false;
+        yield return new WaitForSeconds (1);
+        player.GetComponent<SpawnEnemies> ().spawnOn = true;
+        player.GetComponent<SpawnEnemies> ().SpawnActive ();
+        canSpawn = false;
+        StopCoroutine (SpawnEnemies ());
+    }
+}
--- Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/ConeCollider.cs
+++ Unity Project/Ninja Outbreak/Assets/Scripts/Rief Scripts/Enemy/ConeCollider.cs
@@ -6,14 +6,22 @@ public class ConeCollider : MonoBehaviour {
     //Deze class moet op de Collider van het object van de Viewpoint (guard).
     public GameObject enemy;
     EnemyVirtual enemyDetection;
+    public bool canSpawn;
 
     void Start() {
         enemyDetection = enemy.transform.GetComponent<EnemyVirtual>();
     }
 	void OnTriggerEnter (Collider player) {
         if (player.tag == "Player") {
-            enemyDetection.onPatrol = false;
-            enemyDetection.detected = true;
+            if (enemy.transform.tag == "SamuraiEnemy") {
+                enemyDetection.onPatrol = false;
+                enemyDetection.detected = true;
+            }
+            if (enemy.transform.tag == "AlarmEnemy") {
+                if (enemy.GetComponent<CameraEnemy> ().canSpawn == true) {
+                    enemy.GetComponent<CameraEnemy> ().detected = true;
+                }
+            }
         }
 	}
     void OnTriggerExit(Collider player) {
@@ -23,11 +31,8 @@ public class ConeCollider : MonoBehaviour {
                 enemyDetection.onPatrol = true;
                 enemy.transform.GetComponent<MainEnemy>().lookAroundCoroutine = true;
             }
-            if(enemy.transform.tag == "SamuraiEnemyRun") {
-
-            }
             if(enemy.transform.tag == "AlarmEnemy") {
-
+                enemy.GetComponent<CameraEnemy> ().detected = false;
             }
         }
     }
