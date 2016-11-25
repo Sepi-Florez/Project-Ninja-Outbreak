149e07bf8a6dcbb0475af24fde73e123740c7761
David Vink <david.vink@hotmail.com>
2016-11-25 15:42:43 +0100

    FOV detection work

--- Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/Enemies/Editor/FovEditor.cs
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/Enemies/Editor/FovEditor.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
-public class FovEditor : MonoBehaviour {
+[CustomEditor(typeof(FieldOfView))]
+public class FovEditor : Editor {
 
+
+    void OnSceneGUI() {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.up, 360, fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow.viewAngle / 2, false);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+    } 
 }
---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/Enemies/FieldOfView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldOfView : MonoBehaviour {
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public float meshResolution;
+    
+    
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+        if (!angleIsGlobal)
+            angleInDegrees += transform.eulerAngles.y;
+        return new Vector3(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0) ;
+    }
+    void Update() {
+        DrawFieldOfView();
+    }
+    void DrawFieldOfView() {
+        int rayCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float rayAngleSize = viewAngle / rayCount;
+        for(int a = 0; a <= rayCount; a++) {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + rayAngleSize * a;
+            Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius,Color.red);
+
+        }
+
+    }
+}
+    
