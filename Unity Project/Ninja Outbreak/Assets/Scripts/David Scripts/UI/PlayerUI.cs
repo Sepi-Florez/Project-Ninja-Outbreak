27675733a5d3d9d758aeb343741c2ee01a586387
David Vink <david.vink@hotmail.com>
2017-01-06 20:12:33 +0100

    Tutorial message work
    They will pop when the player walks into a tut trigger. in the tut
    trigger object you will be able to put in a string ( message ) and life
    span of the message.

---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/UI/PlayerUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+    GameObject tutorialBalloon;
+    GameObject tutorialText;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public IEnumerator Tutorial (string message,float alive) {
+        tutorialBalloon.SetActive(true);
+        tutorialText.SetActive(true);
+        tutorialText.GetComponent<Text>().text = message;
+        yield return new WaitForSeconds(alive);
+        tutorialBalloon.SetActive(false);
+        tutorialText.SetActive(false);
+
+    }
+}
---
+++ Unity Project/Ninja Outbreak/Assets/Scripts/David Scripts/UI/tut.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tut : MonoBehaviour {
+    public string tutorialMessage;
+    public float tutorialLife;
+    void onTriggerEnter(Collision player) {
+        if(player.transform.tag == "Player") {
+            //player.transform.GetComponent<PlayerUI>().StartCoroutine(Tutorial(tutorialMessage, tutorialLife));
+        }
+    }
+}
