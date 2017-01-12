511064d10505d72e17d43ac80b32e7d3f64830c4
WalterAHulsebos <treemsi@hotmail.com>
2017-01-12 12:58:41 +0100

    Deleted and replaced 3D objects

---
+++ Unity Project/Ninja Outbreak/Assets/3D objects/Main_Character/PlayerAnims.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerAnims : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > .1f) { transform.localScale = new Vector3(1,1,1);}
+        else if (Input.GetAxis("Horizontal") < -.1f) { transform.localScale = new Vector3(1, 1, -1); }
+
+        if (Input.GetAxis("Horizontal") > .1f || Input.GetAxis("Horizontal") < -.1f) { anim.SetBool("Moving", true); }
+        else { anim.SetBool("Moving", false); }
+
+        if (Input.GetButton("Crouch")) { anim.SetBool("Crouch", true); }
+        else { anim.SetBool("Crouch", false); }
+
+        if (Input.GetButton("Jump")) { anim.SetBool("InAir", true); }
+        else { anim.SetBool("InAir", false); }
+    }
+}
